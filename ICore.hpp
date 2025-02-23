1f256b3899e1e0187dd798d64f937d1421c78bab
Chouteau Perry <perry.chouteau@epitech.eu>
2025-02-23 19:02:49 +0100

    feat(icore/iengine): define icore & iengine as interface to help creating game (associate with lot of other tools I could provide like ECS/BSD/ CustomisableEngine. simple Engine or any game/project you wanna build easily

---
+++ ICore.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file ICore.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef ICORE_HPP
+#define ICORE_HPP
+
+/**
+ * @brief define the core interface
+ * @interface ICore
+ */
+class ICore {
+    public:
+        virtual ~ICore() = default;
+
+        virtual int start() = 0;    
+};
+
+/**
+ * @brief define the engine interface
+ * @interface Engine
+ */
+class IEngine : public ICore {
+    public:
+        virtual ~IEngine() = default;
+
+    protected:
+
+        virtual void initHandler() = 0;
+        virtual void destroyHandler() = 0;
+
+        virtual void eventHandler() = 0;
+        virtual void updateHandler() = 0;
+        virtual void displayHandler() = 0;
+
+};
+
+#endif // ICORE_HPP
